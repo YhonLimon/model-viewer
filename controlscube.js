92aef86f4881528d869384c239207249a48771a2
Jhon Alfredo Silva Alé <36046618+YhonLimon@users.noreply.github.com>
2021-11-06 11:30:29 -0300

    Update controlscube.js

--- controlscube.js
+++ controlscube.js
@@ -1,26 +1,26 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
       
     var model = document.getElementById('cube01')
-    var Running = document.getElementById('0')
-    var Dance = document.getElementById('1')
-    var Death = document.getElementById('2')
+    var Running = document.getElementById('[Esconder acción].001_Cube.001')
+    var Dance = document.getElementById('[Esconder acción].002_Cube.001')
+    var Death = document.getElementById('[Esconder acción]_Cube.001')
     var box3 = document.getElementById('box3')
 
     Running.addEventListener('click', () =>{
 
-      model.setAttribute('animation-name', '0')
+      model.setAttribute('animation-name', '[Esconder acción].001_Cube.001')
 
     })
 
     Dance.addEventListener('click', () =>{
 
-      model.setAttribute('animation-name', '1')
+      model.setAttribute('animation-name', '[Esconder acción].002_Cube.001')
 
     })
 
     Death.addEventListener('click', () =>{
 
-      model.setAttribute('animation-name', '2')
+      model.setAttribute('animation-name', '[Esconder acción]_Cube.001')
 
     })
 
