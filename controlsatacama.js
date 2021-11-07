26d8ec7c42be0440a57b676cdb68fd134f240d3b
Jhon Alfredo Silva Alé <36046618+YhonLimon@users.noreply.github.com>
2021-11-07 01:14:21 -0300

    Create controlsatacama.js

---
+++ controlsatacama.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+      
+    var model = document.getElementById('miniatacama')
+    var uno = document.getElementById('uno')
+    var dos = document.getElementById('dos')
+    var tres = document.getElementById('tres')
+    var cuatro = document.getElementById('cuatro')
+
+    uno.addEventListener('click', () =>{
+
+      model.setAttribute('animation-name', 'Robot_Dance_RobotArmature.001')
+
+    })
+
+    dos.addEventListener('click', () =>{
+
+      model.setAttribute('animation-name', 'Robot_Jump')
+
+    })
+
+    tres.addEventListener('click', () =>{
+
+      model.setAttribute('animation-name', 'Robot_Running')
+
+    })
+
+    cuatro.addEventListener('click', () =>{
+
+      model.setAttribute('animation-name', 'Robot_Idle_RobotArmature.001')
+
+    })
+        
+});
