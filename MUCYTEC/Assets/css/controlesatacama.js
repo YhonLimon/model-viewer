b3ec94e0ea60bc2b0acc49fa99d67980ebafb706
Jhon Alfredo Silva Alé <36046618+YhonLimon@users.noreply.github.com>
2025-07-23 17:31:57 -0400

    Create controlesatacama.js

---
+++ MUCYTEC/Assets/css/controlesatacama.js
@@ -0,0 +1,18 @@
+documento .addEventListener ( "DOMContentLoaded" , función ( evento ) ) {   
+      
+     modelo  var =  documento . getElementById ( 'ninguno' ) 
+    var  cero  =  documento . getElementById ( 'Animation_all' ) 
+    var  uno  =  documento . getElementById ( 'uno' ) 
+    var  dos  =  documento . getElementById ( 'dos' ) 
+    var  tres  =  documento . getElementById ( 'tres' ) 
+    var  cuatro  =  documento . getElementById ( 'cuatro' )
+
+     cero .addEventListener ( 'clic' , () = > {  
+
+      modelo .setAttribute ( 'nombre- de -animación' , 'Animación_todas' ) 
+
+    })
+    
+   
+        
+});
